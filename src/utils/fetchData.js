9b5a17f36b799a99cff32a54ae4c20bc6bcbba98
dndyRabbit <dendybgt@gmail.com>
2022-08-14 17:53:41 +0700

    Offline Mode

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,23 +1,20 @@
 import axios from "axios";
 
 export const getDataAPI = async (url, token) => {
-  const res = await axios.get(
-    `${process.env.REACT_APP_HEROKU_URL}/api/${url}`,
-    {
-      headers: {
-        Authorization: token,
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      withCredentials: true,
-    }
-  );
+  const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/${url}`, {
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    withCredentials: true,
+  });
   return res;
 };
 
 export const postDataAPI = async (url, post, token) => {
   const res = await axios.post(
-    `${process.env.REACT_APP_HEROKU_URL}/api/${url}`,
+    `${process.env.REACT_APP_BASE_URL}/api/${url}`,
     post,
     {
       headers: {
@@ -33,7 +30,7 @@ export const postDataAPI = async (url, post, token) => {
 
 export const putDataAPI = async (url, post, token) => {
   const res = await axios.put(
-    `${process.env.REACT_APP_HEROKU_URL}/api/${url}`,
+    `${process.env.REACT_APP_BASE_URL}/api/${url}`,
     post,
     {
       headers: {
@@ -49,7 +46,7 @@ export const putDataAPI = async (url, post, token) => {
 
 export const patchDataAPI = async (url, post, token) => {
   const res = await axios.patch(
-    `${process.env.REACT_APP_HEROKU_URL}/api/${url}`,
+    `${process.env.REACT_APP_BASE_URL}/api/${url}`,
     post,
     {
       headers: {
@@ -65,7 +62,7 @@ export const patchDataAPI = async (url, post, token) => {
 
 export const deleteDataAPI = async (url, token) => {
   const res = await axios.delete(
-    `${process.env.REACT_APP_HEROKU_URL}/api/${url}`,
+    `${process.env.REACT_APP_BASE_URL}/api/${url}`,
     {
       headers: {
         Authorization: token,
